64d2ef1b018e3a247a25ab042fb098edef78c1d7
Kiss Monica Cristina <71024554+MonicaCristinaKiss@users.noreply.github.com>
2020-09-09 19:51:49 +0300

    Add files via upload

---
+++ CProgramming1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	printf("Hello world!\n");
+	//comment in one line
+	printf("Monica is awesome\t");
+	printf("Monica is cool\n");
+	/* comment in
+	multiple lines
+	*/
+	printf("%s is cool.\n","Monica");
+	printf("%s is the best %s ever.\n","Monica","programmer");
+	printf("%d flowers\n",7);
+	printf("%f\n",3.1415926535);
+	printf("%.2f\n",3.1415926535);
+	printf("%.4f\n",3.1415926535);
+	printf("%.8f\n",3.1415926535);
+	return 0;	
+}
