df9ad27bfa6d2f191ddaa9bbd523a5c41693ccdc
Kiss Monica Cristina <71024554+MonicaCristinaKiss@users.noreply.github.com>
2020-09-23 10:43:51 +0300

    Add files via upload

---
+++ StringsPointers.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+	char prop1[]="Ana are mere.";
+	// propozitie="Monica are mere."; - eroare, prop1 e pointer
+	// propozitie[]="Monica are mere."; - eroare
+	strcpy(prop1,"Monica are mere.");
+	prop1[11]='p';
+	printf("prop1\t%s\t%p\n", prop1,prop1);
+	printf("prop1[0]\t%c\t%p\n",prop1[0],prop1[0]);
+	printf ("p,&prop1[0]\t%p\n",&prop1[0]);
+	
+	char *prop2="Oana are stilou.";
+	printf ("\ns,prop2\t\t%s\n",prop2);
+	printf ("s,*&prop2\t%s\n",*&prop2);
+	printf ("p,&prop2\t%p\n",&prop2);
+	printf ("p,prop2\t\t%p\n",prop2);
+	printf ("p,&prop2[0]\t%p\n",&prop2[0]);
+	
+	printf ("c,prop2[0]\t%c\n",prop2[0]);
+	printf ("p,prop2[0]\t%p\n",prop2[0]);	
+	printf ("c,*prop2\t%c\n",*prop2);
+	printf ("p,*prop2\t%p\n",*prop2);
+
+	prop2="Ana are creion.";
+	printf ("\ns,prop2\t\t%s\n",prop2);
+	printf ("s,*&prop2\t%s\n",*&prop2);
+	printf ("p,&prop2\t%p\n",&prop2);
+	printf ("p,prop2\t\t%p\n",prop2);
+	printf ("p,&prop2[0]\t%p\n",&prop2[0]);
+
+	printf ("c,prop2[0]\t%c\n",prop2[0]);
+	printf ("p,prop2[0]\t%p\n",prop2[0]);	
+	printf ("c,*prop2\t%c\n",*prop2);
+	printf ("p,*prop2\t%p\n",*prop2);
+	
+return 0;
+}
