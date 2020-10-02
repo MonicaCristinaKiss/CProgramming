d7c6c1240d2299c521fa3874a73fc1bb63b8b0cd
Kiss Monica Cristina <71024554+MonicaCristinaKiss@users.noreply.github.com>
2020-10-02 12:12:27 +0300

    Add files via upload

---
+++ Functions.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void printSomething();
+int a=6; //global variable
+
+int main() {
+	int b=4; //local variable
+	printf("a este %d\n",a);
+	printSomething();
+return 0;
+}
+
+void printSomething() {
+	printf("This is a function.\n");
+	printf("a este %d\n",a);
+	return;
+}
